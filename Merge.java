ace7e6773b7a1a3fcbf541560311a79f770c36b4
lilkho <mkhondker@stuy.edu>
2019-03-15 14:31:33 -0400

    starting merge

---
+++ Merge.java
@@ -0,0 +1,10 @@
+public class Merge {
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data) {
+    
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
