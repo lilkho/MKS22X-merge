41f3df1ba888dda8df171ac8cc3f5f617671d85f
lilkho <mkhondker@stuy.edu>
2019-03-18 14:51:46 -0400

    made a converge method

--- Merge.java
+++ Merge.java
@@ -8,20 +8,30 @@ public class Merge {
   }
   private static void mergesort(int[]data,int lo,int hi) {
     if (lo>=hi) return;
-    int[]temp=data;
-    System.out.println(Arrays.toString(temp));
-    mergesort(temp,lo,mid(lo+1,hi+1));
-    mergesort(temp,mid(lo+1,hi+1),hi);
+    System.out.println(lo+" "+hi);
+    System.out.println(mid(lo,hi));
+    int[] con = converge(data,lo,hi);
+    System.out.println(Arrays.toString(con));
+    mergesort(data,lo,mid(lo,hi));
+    mergesort(data,mid(lo,hi)+1,hi);
+    merge(data,lo,hi);
+  }
+  private static void merge(int[] data,int lo,int hi) {
+
   }
   private static int mid(int lo,int hi) {
-    if (hi-lo%2==0) return hi-lo/2;
-    else return hi-lo/2 +1;
+    return (hi-lo)/2;
   }
-
+  private static int[] converge(int[] data,int lo, int hi) {
+    int[] con = new int[hi-lo];
+    for (int i=0;i<hi-lo;i++) {
+      con[i]=data[lo+i];
+    }
+    return con;
+  }
   public static void main(String[] args) {
     int[] data = {38,27,43,3,9,82,10};
     mergesort(data);
-    System.out.println(7/2);
     System.out.println(Arrays.toString(data));
   }
 }
